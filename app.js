6e7ac1da9b37df1173b8ed1d689c3d293e52b8d4
dcamriff <driffelatl@gmail.com>
2018-01-22 11:16:57 -0500

    Set up Express to serve static React files. Created postinstall scrit  for Heroku to build static React app.

--- app.js
+++ app.js
@@ -8,15 +8,19 @@ var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
 const mongoose = require('mongoose')
 
-var index = require('./routes/index');
-var users = require('./routes/users');
-
 var app = express();
 
 // DB setup
 mongoose.Promise = global.Promise
 mongoose.connect(process.env.MONGODB_URI, {
 	useMongoClient: true
+})
+
+// server static react files
+app.use(express.static(__dirname + '/client/build/'))
+
+app.get('/', (req,res) => {
+  res.sendFile(__dirname + '/client/build/index.html')
 })
 
 const connection = mongoose.connection
@@ -41,8 +45,6 @@ app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.use('/', index);
-app.use('/users', users);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
