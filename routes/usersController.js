3cb3a4b0f491c8b449aabdc03d3b3f72f29e5dc6
dcamriff <driffelatl@gmail.com>
2018-01-23 17:09:39 -0500

    Added code for routes: New, Delete, and Update user

--- routes/usersController.js
+++ routes/usersController.js
@@ -1,12 +1,53 @@
 const express = require('express')
-const { User } = require('../db/models/User')
 const router = express.Router()
+const User = require('../db/models/User')
 
-router.get('/', (req, res) => {
-    User.find().then(users => {
+// GET USERS LISTING
+router.get('/', async (req, res) => {
+    try {
+        console.log('Get All Users')
+        const users = await User.find({})
         res.json(users)
-    })
-    .catch((err) => console.log(err))
+    }   catch (error) {
+        console.log(error)
+        res.sendStatus(500)
+    }
+})
+    
+// CREATE A NEW USER
+router.post('/', async (req, res) => {
+    try {
+        console.log('Create a User')
+        const newUser = await User.find({})
+        res.json(newUser)
+    }   catch (error) {
+        console.log(error)
+        res.sendStatus(500)
+    }
+})
+
+// DELETE A USER
+router.delete('/:userId', async (req, res) => {
+    try {
+        await User.findByIdAndRemove(req.params.ideaId)
+        res.sendStatus(200)
+    }   catch (error) {
+        console.log(error)
+        rse.sendStatus(500)
+    }
+})
+
+// EDIT A USER
+router.patch('/:userId', async (req, res) => {
+    try {
+        const updatedUser = 
+        await User.findByIdAndUpdate(req.params.ideaId, req.body, {new: true})
+
+        res.json(updatedUser)
+    }   catch (error) {
+        console.log(error)
+        res.sendStatus(500)
+    }
 })
 
 module.exports = router
