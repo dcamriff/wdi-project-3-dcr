7f11b67779bce049936492570935f45710a69b44
dcamriff <driffelatl@gmail.com>
2018-01-23 17:08:40 -0500

    Added starting code for controllers in Express app

--- app.js
+++ app.js
@@ -1,27 +1,18 @@
 require('dotenv').config()
 
-var express = require('express');
-var path = require('path');
-var favicon = require('serve-favicon');
-var logger = require('morgan');
-var cookieParser = require('cookie-parser');
-var bodyParser = require('body-parser');
+var express = require('express')
+var path = require('path')
+var favicon = require('serve-favicon')
+var logger = require('morgan')
+var cookieParser = require('cookie-parser')
+var bodyParser = require('body-parser')
 const mongoose = require('mongoose')
 
-var app = express();
+var app = express()
 
 // DB setup
 mongoose.Promise = global.Promise
-mongoose.connect(process.env.MONGODB_URI, {
-	useMongoClient: true
-})
-
-// server static react files
-app.use(express.static(__dirname + '/client/build/'))
-
-app.get('/', (req,res) => {
-  res.sendFile(__dirname + '/client/build/index.html')
-})
+mongoose.connect(process.env.MONGODB_URI)
 
 const connection = mongoose.connection
 connection.on('connected', () => {
@@ -33,35 +24,57 @@ connection.on('error', (err) => {
   console.log('Mongoose default connection error: ' + err)	
 })
 
+// server static react files
+app.use(express.static(__dirname + '/client/build/'))
+
+app.get('/', (req,res) => {
+  res.sendFile(__dirname + '/client/build/index.html')
+})
+
+
 // view engine setup
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'hbs');
+app.set('views', path.join(__dirname, 'views'))
+app.set('view engine', 'hbs')
 
 // uncomment after placing your favicon in /public
 //app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
 app.use(logger('dev'));
 app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.urlencoded({ extended: false }))
 app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static(path.join(__dirname, 'public')))
+
+const methodOverride = require('method-override')
+app.use(methodOverride('_method'))
+
+// Registering Controllers
+const usersController = require('./routes/usersController')
+app.use('/api/users', usersController)
+
+// const choresController = require('./routes/choresController')
+// app.use('/api/chores', choresController)
+
+// const rewardsController = require('./routes/rewardsController')
+// app.use('/api/chores', rewardsController)
+
 
 
 // catch 404 and forward to error handler
-app.use(function(req, res, next) {
-  var err = new Error('Not Found');
-  err.status = 404;
-  next(err);
+app.use(function(req, res) {
+  var err = new Error('Not Found')
+  err.status = 404
+  next(err)
 });
 
 // error handler
-app.use(function(err, req, res, next) {
+app.use(function(err, req, res) {
   // set locals, only providing error in development
   res.locals.message = err.message;
-  res.locals.error = req.app.get('env') === 'development' ? err : {};
+  res.locals.error = req.app.get('env') === 'development' ? err : {}
 
   // render the error page
-  res.status(err.status || 500);
-  res.render('error');
+  res.status(err.status || 500)
+  res.render('error')
 });
 
-module.exports = app;
+module.exports = app
