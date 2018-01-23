ec2f485bb19369e664346a955f4ee0331076b7cf
dcamriff <driffelatl@gmail.com>
2018-01-23 17:27:22 -0500

    Adding some starter code to client app.js

--- client/src/App.js
+++ client/src/App.js
@@ -1,21 +1,27 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from 'react'
+import { BrowserRouter as Router, Route, Switch, Link } from 'react-router-dom'
+import styled from 'styled-components'
+import HomePage from './components/HomePage'
+import UserPage from './components/UserPage'
+import RewardsPage from './components/RewardsPage'
 
 class App extends Component {
   render() {
     return (
+      <Router>
       <div className="App">
         <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
+          <h1 className="App-title">Hello World!</h1>
         </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
       </div>
+      <div>
+        <Switch>
+          {/* <Route exact path= */}
+        </Switch>
+      </div>
+      </Router>
     );
   }
 }
 
-export default App;
+export default App
