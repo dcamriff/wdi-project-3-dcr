6f63795f22be0fc5680999dbddae44541ed6e6b6
dcamriff <driffelatl@gmail.com>
2018-01-24 10:20:48 -0500

    Created a UserShow page to show details for individual users.

---
+++ client/src/components/UserShow.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react'
+
+class UserShow extends Component {
+
+    render () {
+        return (
+            <div>This is a Show Page for individual user.</div>
+        )
+    }
+}
+
+export default UserShow
