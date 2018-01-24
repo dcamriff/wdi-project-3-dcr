68948a0defc4c4dc588112f6519c039a762f2742
dcamriff <driffelatl@gmail.com>
2018-01-23 20:52:08 -0500

    Added UserList.js file

---
+++ client/src/components/UserList.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react'
+
+class UserList extends Component {
+
+    render () {
+        render (
+            <div>
+                <h1>UserList Component</h1>
+            </div>
+        )
+    }
+
+}
+
+export default UserList
