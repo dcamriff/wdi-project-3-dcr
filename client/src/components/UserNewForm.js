fa09aa27c7609b366549ff4042dcb60393c43574
dcamriff <driffelatl@gmail.com>
2018-01-25 11:25:32 -0500

    Working to add new user to users page and db

--- client/src/components/UserNewForm.js
+++ client/src/components/UserNewForm.js
@@ -1,28 +1,78 @@
-import React, { Component } from 'react'
+import React, {Component} from 'react'
 
 class UserNewForm extends Component {
-    constructor (props) {
+    constructor(props) {
         super(props)
 
         this.defaultState = {
             newUser: {
                 firstName: '',
-                birthMonth: 'January',
-                timestamp: Date.now(),
-            },
+                birthMonth: '',
+                timestamp: Date.now()
+            }
         }
-        this.state = {...this.defaultState}
-
-        // _handleInputChange = (event) => {
-        //     const attribute = event.target.
-        // }
+        this.state = {
+            ...this.defaultState
+        }
     }
 
-    render () {
-        return (
-            <div>Add User Form</div>
-        )
-    }
-}
+        _handleNewUserChange = (event) => {
+            const attributeName = event.target.name
+            let attributeValue = event.target.value
+            const newUser = {
+                ...this.state.newUser
+            }
+            newUser[attributeName] = attributeValue
+            this.setState({newUser})
+        }
 
-export default UserNewForm
+        _resetForm = () => {
+            const newUser = {...this.defaultState.newUser}
+            this.setState({newUser})
+        }
+
+        _addNewUser = (event) => {
+            event.preventDefault()
+            this.props.addNewUser(this.state.newUser)
+            this._resetForm()
+        }
+
+
+        render() {
+            return (
+                <div>
+                    <h1>Add User Form</h1>
+                    <div>
+                        <form onSubmit={this._addNewUser}>
+                        <input
+                            name="firstName"
+                            type="text"
+                            placeholder="First Name"
+                            value={this.state.newUser.firstName}
+                            onChange={this._handleNewUserChange}/>
+                        <br/>
+                        <input
+                            name="birthMonth"
+                            type="text"
+                            placeholder="Birth Month"
+                            value={this.state.newUser.birthMonth}
+                            onChange={this._handleNewUserChange}/>
+                        <br/>
+                        <input
+                            name="profilePic"
+                            type="text"
+                            placeholder="URL to Profile Pic"
+                            value={this.state.newUser.profilePic}
+                            onChange={this._handleNewUserChange}/>
+                            <input type="submit"
+                            value="Add New User"/>
+
+
+                            </form>
+                    </div>
+                </div>
+            )
+        }
+    }
+
+    export default UserNewForm
--- client/src/components/UserPage.js
+++ client/src/components/UserPage.js
@@ -1,8 +1,9 @@
-import React, { Component } from 'react'
+import React, {Component} from 'react'
 import axios from 'axios'
 import styled from 'styled-components'
 
 import UserList from './UserList'
+import UserNewForm from './UserNewForm'
 
 class UserPage extends Component {
 
@@ -10,68 +11,49 @@ class UserPage extends Component {
         users: []
     }
 
-    async componentWillMount () {
+    async componentWillMount() {
         const response = await axios.get('/api/users')
         // console.log('USERPAGE', response.data)
         this.setState({users: response.data})
 
     }
 
-    createUser = async () => {
-        const response = await axios.get(`/api/users`)
-        const newUser = response.data
-
-        const newUsers = [this.state.users]
-        newUsers.unshift(newUser)
-        this.setState({users: newUsers})
+    addNewUser = (newUser) => {
+        const users = [...this.state.users]
+        users.push(newUser)
+        this.setState({users})
     }
 
-    deleteUser = async (user) => {
-        try {
-            await axios.delete(`/api/users/${user._id}`)
 
-            const indexToDelete = this.state.users.indexOf(user)
-            const newUsers = [...this.state.users]
-            newUsers.splice(indexToDelete, 1)
-            this.setState({users: newUsers})
-        } catch (error) {
-            console.log(error)
-        }
-    }
-
-    render () {
+    render() {
         const users = this.state.users
         console.log('Under render', users)
 
         return (
             <div>
-
+                <h1>UserPage: showing all users</h1>
+                <button onClick={this.createUser}>New User</button>
+                <UserNewForm />
                 <div>
-                    <h1>UserPage: showing all users</h1>
-                    <button onClick={this.createUser}>New User</button>
-                </div>
-
-
-                    <div>
-                        {users.map((user) => {
-                            return (
-                                <div>
-                                    {user.firstName}
-                                    {user.birthMonth}
-                                    <img 
-                                    width="50"
-                                    src={user.profilePic} alt=""/>
-                                </div>
-                                
+                    {users.map((user) => {
+                        return (
+                            <div>
+                                {user.firstName}
+                                {user.birthMonth}
+                                <img width="50" src={user.profilePic} alt=""/> {/* <div>
+                                    <UserNewForm />
+                                    </div> */}
+                            </div>
                             )
-                        })}
-                    </div>
-                    {/* users={this.state.users}
+                        })
+                    }
+                </div>
+                {/* users={this.state.users}
                     handleChange={this.handleChange}
                     updateUser={this.updateUser}
                     deleteUser={this.deleteUser}
                     /> */}
-    
+
             </div>
 
         )
