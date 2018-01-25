9a0e2503e07d88ca4ea51a48f453a0fe9f3cd687
dcamriff <driffelatl@gmail.com>
2018-01-24 19:16:11 -0500

    testing data on User component

--- client/src/components/User.js
+++ client/src/components/User.js
@@ -1,17 +1,40 @@
-import React, { Component } from 'react'
+import React from 'react'
+import styled from 'styled-components'
 
-class User extends Component {
+// class User extends Component {
 
-    render () {
-        return (
-            <div>
-                <img src="#" alt=""/>
-                <strong>{this.props.firsName}</strong>
-                <p>{this.props.birthMonth}</p>
-            </div>
-        )
-    }
+//     render () {
+//         return (
+//             <div>
+//                 <img src={this.props.profilePic} alt="{this.props.firstName}'s profile pic"/>
+//                 <strong>{this.props.firstName}</strong>
+//                 <p>{this.props.birthMonth}</p>
+//             </div>
+//         )
+//     }
 
+// }
+
+const User = (props) => {
+    return (
+        <div className="userWrapper">
+            <input 
+            type="text"
+            name="firstName"
+            value={props.user.firstName}
+            onChange={(event) => props.handleChange(props.user, event)}
+            onBlur={() => {props.updateUser(props.user)}}/>
+
+            <text 
+            name="birthMonth"
+            value={props.user.birthMonth}
+            onChange={(event) => props.handleChange(props.user, event)}
+            onBlur={() => {props.updateUser(props.user)}}/>
+
+            <button onClick={() => {props.deleteUser(props.user)}}>Delete User</button>
+
+        </div>
+    )
 }
 
 export default User
